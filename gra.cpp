8630b273ce3764354428d64d5124631b771ee634
Davwidos <d.jurkowski015@gmail.com>
2020-06-14 16:51:50 +0200

    com

--- gra.cpp
+++ gra.cpp
@@ -277,6 +277,7 @@ void Gra::recive(QString s,QString nadawca)
         drugigracz->k2=b;
         }
         connect(gracz->k1,SIGNAL(clicked(Karta*)),gracz,SLOT(zagraj(Karta*)));
+        gracz->k1->setY(height()/2-gracz->k1->boundingRect().height());
         gracz->k1->setX(width()/2-gracz->k1->boundingRect().width());
         scene->addItem(gracz->k1);
     }
